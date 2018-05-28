e0bcabe170ce8c540b62f431e7c8abfa7a8cfeac
Rob <robdwilliams85@gmail.com>
2018-05-28 11:53:04 +0100

    Added previous_hash and defined blockchain class with genesis block

--- block.py
+++ block.py
@@ -1,7 +1,15 @@
+import json
+from hashlib import sha256
+import time
+
 class Block:
-    def __init__(self, index, transactions, timestamp):
+
+    def __init__(self, index, transactions, timestamp, previous_hash):
         self.index = []
         self.transactions = transactions
         self.timestamp = timestamp
+        self.previous_hash = previous_hash
 
-    
+    def compute_hash(block):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
+        return sha256(block_string.encode()).hexdigest()
