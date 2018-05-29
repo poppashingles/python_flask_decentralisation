12bb5678b5c6e3b7e5ff35dd0a8370e459217f30
Rob <robdwilliams85@gmail.com>
2018-05-29 14:52:55 +0100

    Defined the submit route in views

--- app/views.py
+++ app/views.py
@@ -7,3 +7,36 @@ from app import app
 CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8000"
 
 posts = []
+
+def fetch_posts():
+    get_chain_address = "{}/chain".format(CONNECTED_NODE_ADDRESS)
+    response = requests.get(get_chain_address)
+    if response.status_code == 200:
+        content = []
+        chain = json.loads(response.content)
+        for block in chain["chain"]:
+            for tx in block["transactions"]:
+                tx["index"] = block["index"]
+                tx["hash"] = block["previous_hash"]
+                content.append(tx)
+
+        global posts
+        posts = sorted(content, key=lambda k: k['timestamp'], reverse = True)
+
+@app.route('/submit', methods=['POST'])
+def submit_textarea():
+    post_content = request.form["content"]
+    author = request.form["author"]
+
+    post_object = {
+        'author': author,
+        'content': post_content,
+    }
+
+    new_tx_address = "{}/new_transaction".format(CONNECTED_NODE_ADDRESS)
+
+    requests.post(new_tx_address,
+                  json=post_object,
+                  headers={'Content-type': 'application/json'})
+
+    return redirect('/')
